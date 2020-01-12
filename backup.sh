6855fb6d6f6af38b7f958287704bb8ce9c61b358
Giuseppe Rota <rota.giuseppe@gmail.com>
2020-01-12 12:35:43 +0100

    upgrade

--- backup.sh
+++ backup.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-rsync -av --delete --delete-excluded --exclude-from=rsync_excludes  --files-from=rsync_sources -r pi@kodi: ./apps/
-cp ./apps/docker-compose.yml ./
+rsync -av --delete --delete-excluded --exclude-from=rsync_excludes  --files-from=rsync_sources -r pi@kodi: ./
